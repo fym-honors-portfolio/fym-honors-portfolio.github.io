3b13d39d46e6cc2754c56d8cb42ff41f6041b319
madigan-99 <fymadigan@outlook.com>
2018-11-25 23:51:27 -0800

    send

--- password.js
+++ password.js
@@ -4,7 +4,7 @@ function password() {
     var ans = "h0n0rs";
     while (count < 100) {
         if (password == ans) {
-            download("documents/autumn-reflection.docx");
+           window.open("https://docs.google.com/document/d/1oQfFP9QBKwIfCvuAvDpQkZTEkbhuod2IgOYl8iJlmS8/edit?usp=sharing");
             break;
         } else {
             password = prompt('Access Denied - Password Incorrect, Please Try Again.');
