ae73e60810657d51dcf03e4c06385e596777f688
madigan-99 <fymadigan@outlook.com>
2018-11-25 22:21:13 -0800

    push

--- password.js
+++ password.js
@@ -1,22 +1,20 @@
 function password() {
-var password = prompt('Please Enter The Password',' ');
-var count  = 0;
-var ans = "h0n0rs";
-while (count < 100) { 
-	if(password == ans){
-		download("documents/autumn-reflection.docx");
-		break;
-	}
-	else{
-	password = prompt('Access Denied - Password Incorrect, Please Try Again.');
-	if(password.toLowerCase() == ans){
-		window.open("https://docs.google.com/document/d/1oQfFP9QBKwIfCvuAvDpQkZTEkbhuod2IgOYl8iJlmS8/edit?usp=sharing");
-		break;
-	}
-	else{
-		count++
-	}
-}
-}
+    var password = prompt('Please Enter The Password', ' ');
+    var count = 0;
+    var ans = "h0n0rs";
+    while (count < 100) {
+        if (password == ans) {
+            download("documents/autumn-reflection.docx");
+            break;
+        } else {
+            password = prompt('Access Denied - Password Incorrect, Please Try Again.');
+            if (password.toLowerCase() == ans) {
+                window.open("https://docs.google.com/document/d/1oQfFP9QBKwIfCvuAvDpQkZTEkbhuod2IgOYl8iJlmS8/edit?usp=sharing");
+                break;
+            } else {
+                count++
+            }
+        }
+    }
 
-} 
+}
